7229793b711e1d4c5e88c880d210dc8d49ca3699
Peter Hinch <peter@hinch.me.uk>
2018-12-06 06:05:43 +0000

    cpython not working

---
+++ cpython/local.py
@@ -0,0 +1,6 @@
+MY_ID = '1\n'  # Must be of form <integer>\n
+PORT = 8123
+TIMEOUT = 1500 # ms. Share between client.py and server.py
+# My boxes
+SERVER = '192.168.0.35'  # Laptop
+#SERVER = '192.168.0.33'  # Pi
