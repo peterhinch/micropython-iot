1ae250a9432b6dafc31e62a0ac09b92cacc8f8c6
Kevin Köck <kevinkk525@users.noreply.github.com>
2019-01-26 23:22:33 +0100

    untested merge of all features and combined interface

--- examples/c_app.py
+++ examples/c_app.py
@@ -41,10 +41,10 @@ class App:
         while True:
             # Attempt to read data: in the event of an outage, .readline()
             # pauses until the connection is re-established.
-            header, line = await self.cl.readline()
+            line = await self.cl.readline()
             data = ujson.loads(line)
             # Receives [restart count, uptime in secs]
-            print('Got', header, data, 'from server app')
+            print('Got', data, 'from server app')
 
     # Send [approx application uptime in secs, (re)connect count]
     async def writer(self):
@@ -61,7 +61,7 @@ class App:
             print('Sent', data, 'to server app\n')
             # .write() behaves as per .readline()
             st = utime.ticks_ms()
-            await self.cl.write(None, ujson.dumps(data))
+            await self.cl.writeline(ujson.dumps(data))
             latency = utime.ticks_ms() - st
             self.latency_added += latency
             self.count += 1
