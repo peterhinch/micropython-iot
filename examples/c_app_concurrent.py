8c226a1746ed668363fe2c239275e69e761d048f
Kevin Köck <kevinkk525@users.noreply.github.com>
2019-01-28 19:28:35 +0100

    concurrent write test

--- examples/c_app_concurrent.py
+++ examples/c_app_concurrent.py
@@ -1,4 +1,4 @@
-# c_app.py Client-side application demo
+# c_app_concurrent.py Client-side application test
 
 # Released under the MIT licence.
 # Copyright (C) Peter Hinch 2019
@@ -11,26 +11,18 @@ from micropython_iot import client
 
 gc.collect()
 import ujson
-# Optional LED. led=None if not required
-from sys import platform
-if platform == 'pyboard':  # D series
-    from pyb import LED
-    led = LED(1)
-else:
-    from machine import Pin
-    led = Pin(2, Pin.OUT, value=1)  # Optional LED
-# End of optionalLED
+from machine import Pin
 
 from . import local
 
 gc.collect()
 
 
-class App(client.Client):
-    def __init__(self, loop, verbose):
+class App:
+    def __init__(self, loop, my_id, server, port, timeout, verbose):
         self.verbose = verbose
-        self.cl = client.Client(loop, local.MY_ID, local.SERVER, local.SSID, local.PW,
-                         conn_cb=self.constate, verbose=verbose, led=led, wdog=False)
+        led = Pin(2, Pin.OUT, value=1)  # Optional LED
+        self.cl = client.Client(loop, my_id, server, port, timeout, self.constate, None, verbose, led)
         loop.create_task(self.start(loop))
         self.latency_added = 0
         self.count = 0
@@ -56,33 +48,39 @@ class App(client.Client):
 
     # Send [approx application uptime in secs, (re)connect count]
     async def writer(self):
-        import utime
         self.verbose and print('Started writer')
-        data = [0, 0, 0]
+        data = [0, 0, 0, 0]
         count = 0
         while True:
+            await self.cl  # prevent outage from creating queue overflow
             data[0] = self.cl.connects
-            data[1] = count
-            count += 1
-            gc.collect()
             data[2] = gc.mem_free()
-            print('Sent', data, 'to server app\n')
-            # .writeline() behaves as per .readline()
-            st = utime.ticks_ms()
-            await self.cl.writeline(ujson.dumps(data))
-            latency = utime.ticks_ms() - st
-            self.latency_added += latency
-            self.count += 1
-            print("Latency:", latency, "Avg Latency:", self.latency_added / self.count)
-            # await asyncio.sleep(5)
+            for i in range(5):
+                data[1] = count
+                count += 1
+                gc.collect()
+                data[3] = i
+                loop.create_task(self._con_write(ujson.dumps(data)))
+            await asyncio.sleep(2)
+            print("Avg Latency:", self.latency_added / self.count)
+            await asyncio.sleep(3)
 
-    def shutdown(self):
-        self.cl.close()  # Shuts down WDT (but not on Pyboard D).
+    async def _con_write(self, data):
+        import utime
+        print('Sent', data, 'to server app\n')
+        st = utime.ticks_ms()
+        await self.cl.writeline(data)
+        latency = utime.ticks_ms() - st
+        self.latency_added += latency
+        self.count += 1
+
+    def close(self):
+        self.cl.close()
 
 
 loop = asyncio.get_event_loop()
-app = App(loop, verbose=True)
+app = App(loop, local.MY_ID, local.SERVER, local.PORT, local.TIMEOUT, True)
 try:
     loop.run_forever()
 finally:
-    app.shutdown()
+    app.close()
